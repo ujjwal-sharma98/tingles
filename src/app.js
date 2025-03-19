1d60bec17bf07f9bb641fa31b9fa62f88d64fa0e
ujjwal <ujjwal.develop@gmail.com>
2025-03-19 23:22:41 +0530

    cors package added

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express();
 const connectDB = require("./database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 require('dotenv').config()
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json())
 app.use(cookieParser());
 
